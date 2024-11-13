7dd21001080c96d16b0781e630a10abd0625faa3
Hea092024 <Hea092024@gmail.com>
2024-11-13 10:43:10 +0100

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Replace the arguments below according to your preference.
 
 // space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("fantasy")
+favouriteMovieGenre("space")
 
 // watermelon, tomato, banana, orange, avocado, blueberry
 favouriteFruit("banana")
@@ -11,7 +11,7 @@ favouriteFruit("banana")
 favouriteMode("dark")
 
 // sharp, soft, round
-favouriteEdgeStyle("soft")
+favouriteEdgeStyle("round")
 
 
 
